f5b993e073eda44d90dce2ebc73148231a502170
Matheus Pereira <matheusdimmy@hotmail.com>
2022-02-21 15:55:06 -0300

    Requisito 2 - implementa a funçao que habilita o botao de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
 
 class Login extends React.Component {
+  state = {
+    isDisabled: true,
+  }
+
+  minCharValidation = ({ target: { value } }) => {
+    const minChar = 3;
+    this.setState({
+      isDisabled: value.length < minChar,
+    });
+  };
+
   render() {
+    const { isDisabled } = this.state;
+
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        Login
+        <br />
+        <input
+          type="text"
+          data-testid="login-name-input"
+          onChange={ this.minCharValidation }
+        />
+        <br />
+        <button
+          type="submit"
+          data-testid="login-submit-button"
+          disabled={ isDisabled }
+        >
+          Entrar
+        </button>
+      </div>
     );
   }
 }
