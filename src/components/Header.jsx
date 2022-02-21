fc8ae08eab715753429b80da20d891170cc60cf9
Matheus Pereira <matheusdimmy@hotmail.com>
2022-02-21 20:23:05 -0300

    Finaliza requisito 3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Header extends React.Component {
+  state = {
+    isLoading: false,
+    name: '',
+  }
+
+  async componentDidMount() {
+    this.setState({ isLoading: true });
+    const { name } = await getUser();
+    this.setState({
+      isLoading: false,
+      name,
+    });
+  }
+
   render() {
+    const { isLoading, name } = this.state;
+
     return (
-      <div data-testid="header-component">Header</div>
+      <header data-testid="header-component">
+        {
+          isLoading
+            ? <Loading />
+            : <div data-testid="header-user-name">{name}</div>
+        }
+      </header>
     );
   }
 }
