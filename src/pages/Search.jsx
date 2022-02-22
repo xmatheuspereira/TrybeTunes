9ebc084709713cb0716131cd5709c70dde0635bc
Matheus Pereira <matheusdimmy@hotmail.com>
2022-02-22 15:59:21 -0300

    Req. 6 - implementa a requisiçao da API

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends React.Component {
   state = {
     isDisabled: true,
+    bandOrArtist: '',
+    albums: [],
   }
 
   minCharValidation = ({ target: { value } }) => {
     const minChar = 2;
     this.setState({
       isDisabled: value.length < minChar,
+      bandOrArtist: value,
     });
+  };
+
+  searchAlbums = async () => {
+    const { bandOrArtist } = this.state;
+    await searchAlbumsAPI(bandOrArtist).then((artistName) => (
+      this.setState({ albums: artistName })));
   };
 
   render() {
@@ -30,6 +40,7 @@ class Search extends React.Component {
           type="button"
           data-testid="search-artist-button"
           disabled={ isDisabled }
+          onClick={ this.searchAlbums }
         >
           Pesquisar
         </button>
