c7bcdb3d3c38687d20da48c1a7a8e22eaa62fbd1
Matheus Pereira <matheusdimmy@hotmail.com>
2022-02-21 21:50:23 -0300

    Requisito 5 OK

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,37 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    isDisabled: true,
+  }
+
+  minCharValidation = ({ target: { value } }) => {
+    const minChar = 2;
+    this.setState({
+      isDisabled: value.length < minChar,
+    });
+  };
+
   render() {
+    const { isDisabled } = this.state;
+
     return (
       <div data-testid="page-search">
         <Header />
+        <br />
+        <input
+          type="search"
+          data-testid="search-artist-input"
+          placeholder="Nome do Artista"
+          onChange={ this.minCharValidation }
+        />
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ isDisabled }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
