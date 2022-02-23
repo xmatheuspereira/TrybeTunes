5984a32a64002f488cd940d884d8cb6e2aa8fdbc
Matheus Pereira <matheusdimmy@hotmail.com>
2022-02-22 21:48:44 -0300

    Req 6 - adiciona capa dos albuns

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from '../components/Loading';
 
 class Search extends React.Component {
   state = {
     isDisabled: true,
     bandOrArtist: '',
     albums: [],
+    isLoading: false,
     fetched: false,
     artistName: '',
   }
@@ -24,35 +26,43 @@ class Search extends React.Component {
   searchAlbums = async () => {
     const { bandOrArtist } = this.state;
 
+    this.setState({ isLoading: true });
+
     await searchAlbumsAPI(bandOrArtist).then((artistName) => (
       this.setState({
         albums: artistName,
         bandOrArtist: '',
+        isLoading: false,
         fetched: true })));
   };
 
   render() {
-    const { isDisabled, bandOrArtist, albums, fetched, artistName } = this.state;
+    const {
+      isDisabled, bandOrArtist, albums, isLoading, fetched, artistName } = this.state;
 
     return (
       <div data-testid="page-search">
         <Header />
         <br />
-        <input
-          type="search"
-          data-testid="search-artist-input"
-          placeholder="Nome do Artista"
-          onChange={ this.minCharValidation }
-          value={ bandOrArtist }
-        />
-        <button
-          type="button"
-          data-testid="search-artist-button"
-          disabled={ isDisabled }
-          onClick={ this.searchAlbums }
-        >
-          Pesquisar
-        </button>
+        { isLoading ? <Loading /> : (
+          <>
+            <input
+              type="search"
+              data-testid="search-artist-input"
+              placeholder="Nome do Artista"
+              onChange={ this.minCharValidation }
+              value={ bandOrArtist }
+            />
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              disabled={ isDisabled }
+              onClick={ this.searchAlbums }
+            >
+              Pesquisar
+            </button>
+          </>
+        )}
         {/* (artistName !== bandOrArtist) utilizado pra impedir que o artistName fosse renderizado enquanto estava digitando no input */}
         {(fetched && albums.length > 0 && artistName !== bandOrArtist) && (
           <h2>{`Resultado de álbuns de: ${artistName}`}</h2>
@@ -63,6 +73,7 @@ class Search extends React.Component {
         {albums.map(({
           collectionId,
           collectionName,
+          artworkUrl100,
         }) => (
           <Link
             data-testid={ `link-to-album-${collectionId}` }
@@ -70,7 +81,9 @@ class Search extends React.Component {
             key={ collectionId }
           >
             <ul>
-              <li>{collectionName}</li>
+              {collectionName}
+              <br />
+              <img src={ artworkUrl100 } alt="album-cover" />
             </ul>
           </Link>
         ))}
