fd673832ab50155763c2a138c1885c2b72741bcf
Matheus Pereira <matheusdimmy@hotmail.com>
2022-02-24 02:00:28 -0300

    Requisito 13 OK

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
 
 class Profile extends React.Component {
+  state = {
+    name: '',
+    email: '',
+    image: '',
+    description: '',
+  };
+
+  async componentDidMount() {
+    const userProfile = await getUser();
+    this.setState({ ...userProfile });
+  }
+
   render() {
+    const { name, email, image, description } = this.state;
+
     return (
-      <div data-testid="page-profile">
+      <>
         <Header />
-      </div>
+        <div data-testid="page-profile">Profile</div>
+        <img data-testid="profile-image" src={ image } alt={ name } />
+        <p>{ name }</p>
+        <p>{ email }</p>
+        <p>{ description }</p>
+        <Link to="/profile/edit">Editar perfil</Link>
+      </>
     );
   }
 }
