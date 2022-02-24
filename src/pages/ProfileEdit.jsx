d767cf53154345b104a61a6ba87863e8cf797718
Matheus Pereira <matheusdimmy@hotmail.com>
2022-02-24 03:32:52 -0300

    Requisito 14 OK

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Redirect } from 'react-router-dom';
 import Header from '../components/Header';
 import Loading from '../components/Loading';
 import { getUser, updateUser } from '../services/userAPI';
 
-class ProfileEdit extends Component {
+class ProfileEdit extends React.Component {
   state = {
     name: '',
     email: '',
